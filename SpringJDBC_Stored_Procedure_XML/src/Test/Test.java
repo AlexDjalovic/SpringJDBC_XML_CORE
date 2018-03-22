3fb93f6806344d168d00a236891f2ae5345a128e
AlexDjalovic <adaleksandardjalovic12@gmail.com>
2018-03-22 22:06:24 +0100

    second commit for stored procedures using spring

--- SpringJDBC_Stored_Procedure_XML/src/Test/Test.java
+++ SpringJDBC_Stored_Procedure_XML/src/Test/Test.java
@@ -11,13 +11,13 @@ import Service.EmpService;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		AbstractApplicationContext con=new ClassPathXmlApplicationContext("springJDBC.xml");
 		EmpService service=(EmpService) con.getBean("empServiceImpl");
-		System.out.println("-----------------");
-	//Employee emp1=service.dohvatiEmplById(1);
-	//System.out.println(emp1);
+		
+		
+	
 		service.prikaziMapu(1);
 	
 		System.out.println("*****");
